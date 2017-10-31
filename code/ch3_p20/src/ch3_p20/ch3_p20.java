9280c65d079a3def02aa7db1edcdcfe4bf491f4c
pandelaz <pandelaz@gmail.com>
2017-11-01 00:00:16 +0800

    first commit

---
+++ code/ch3_p20/src/ch3_p20/ch3_p20.java
@@ -0,0 +1,22 @@
+package ch3_p20;
+public class ch3_p20 {
+	public static void main(String[] args) {
+		Car car1;
+		car1 = new Car();
+		
+		car1.num = 1234;
+		car1.gas =-10;
+		car1.show();
+	}
+}
+//Car類別
+class Car
+{
+	int num;
+	double gas;
+	
+	void show() {
+		System.out.println("車號是"+num);
+		System.out.println("汽油量是"+gas);
+	}
+}
---
+++ code/ch3_p5/src/ch3_p5/ch3_p5.java
@@ -0,0 +1,16 @@
+package ch3_p5;
+
+public class ch3_p5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car car1;
+		car1 = new Car();
+		
+	}
+
+}
+
+class Car{
+	//Car類別
+}
